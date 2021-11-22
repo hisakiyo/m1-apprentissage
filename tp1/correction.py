274012d3b0cee501cca33a86a9c216fa72916501
hisakiyo <hisakiyo.games@gmail.com>
2021-11-22 15:38:35 +0100

    TP1: computing errors + print_r_squared

--- tp1/correction.py
+++ tp1/correction.py
@@ -38,19 +38,42 @@ def show_polynomes(models, degrees, x, y):
         y_pred = models[count].predict(x)
         x = x.reshape(-1)
         x,y_pred = smooth_curve(x, y_pred)
+        # reshape x
         x = x.reshape(-1, 1)
-        plt.plot(x, y_pred, color=colors[count], linewidth=3, label="degree %d" % degree)
+        plt.plot(x, y_pred, color=colors[count], linewidth=2, label="degree %d" % degree)
         count += 1
     plt.grid(True)
     plt.legend()
     plt.show()
 
+def compute_errors(models, degrees, x, y):
+    counts = 0
+    for model in models:
+        prediction = model.predict(x)
+        print("Degre: %2.f : " % degrees[counts], "MSE: %.2f" % np.mean((prediction - y)**2))
+        counts += 1
+
+def print_r_squared(models, degrees, x, y):
+    counts = 0
+    for model in models:
+        prediction = model.predict(x)
+        print("Degre: %2.f : " % degrees[counts], "R2: %.2f" % model.score(x, y))
+        counts += 1
+
 def main():
     x, y = generate_train_data(15)
     degrees = [1, 3, 6, 9, 12]
-    # reshape x
     x = x.reshape(-1, 1)
-    show_polynomes(training(degrees, x, y), degrees, x, y)
+    models = training(degrees, x, y)
+    show_polynomes(models, degrees, x, y)
+    # Test data
+    x_test, y_test = generate_train_data(50)
+    x_test = x_test.reshape(-1, 1)
+    show_polynomes(models, degrees, x_test, y_test)
+    compute_errors(models, degrees, x_test, y_test)
+    print_r_squared(models, degrees, x_test, y_test)
+    
+
 
 if __name__ == '__main__':
     main()
