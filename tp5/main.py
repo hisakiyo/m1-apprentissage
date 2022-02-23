133b6da6672e766fd84ccd8de357c44e50e48535
hisakiyo <hisakiyo.games@gmail.com>
2022-02-23 14:21:27 +0100

    First QLearning

--- tp5/main.py
+++ tp5/main.py
@@ -1,16 +1,53 @@
 import random
 
+class Agent:
+    def __init__(self, vlambda, ile):
+        self.vlambda = vlambda
+        self.q = []
+        self.ile = ile
+        self.init_matrix()
+    
+    def init_matrix(self):
+        self.q = self.ile.calculer_matrice_etat_actions()
+        # For into self.q and set all values to 0 
+        for i in range(self.ile.taille):
+            for j in range(self.ile.taille):
+                for k in range(4):
+                    if self.q[i][j][k] != -1:
+                        self.q[i][j][k] = 0
 
-# Classe Ile représentant une ile du jeu avec une matrice
+    def update_q(self, etat, action, reward):
+        self.q[etat[0]][etat[1]][action] += self.vlambda * (reward - self.q[etat[0]][etat[1]][action])
+        print('ajouté :', self.vlambda * (reward - self.q[etat[0]][etat[1]][action]))
+        return self.ile.etat_suivant(etat, action)
+
+    # In Qtable, find the best action for the current state
+    def choisir_action(self, etat):
+        # Get possible actions
+        action = self.q[etat[0]][etat[1]].index(max(self.q[etat[0]][etat[1]]))
+        if self.q[etat[0]][etat[1]][action] == 0:
+            action = random.randint(0, 3)
+        print('State :', self.q[etat[0]][etat[1]])
+        if action == 0:
+            return 'N'
+        elif action == 1:
+            return 'S'
+        elif action == 2:
+            return 'E'
+        elif action == 3:
+            return 'O'
+
+
+
 class Ile:
-    def __init__(self, taille=10, nb_rhum=1, position_joueur=[0, 0]):
+    def __init__(self, taille=10, nb_rhum=3, position_joueur=[0, 0]):
         self.matrice = []
         self.taille = taille
         self.nb_rhum = nb_rhum
         self.nb_tresor = 1
         self.position_joueur = position_joueur
         self.steps = 0
-        self.max_steps = 20
+        self.max_steps = 20000
     
     def init_matrice(self):
         for i in range(self.taille):
@@ -100,29 +137,48 @@ class Ile:
                             matrice_etat_actions[i][j].append(self.matrice[i][j - 1])
                     else:
                         matrice_etat_actions[i][j].append(-1)
-
-
         return matrice_etat_actions
 
-    # Fonction qui retourne un nouvel état en fonction d'un état et d'une action
-    
+    def etat_suivant(self, etat, action):
+        if action == 'N':
+            return etat[0] - 1, etat[1]
+        elif action == 'S':
+            return etat[0] + 1, etat[1]
+        elif action == 'E':
+            return etat[0], etat[1] + 1
+        elif action == 'O':
+            return etat[0], etat[1] - 1
+        else:
+            return etat
 
     def mouvement_joueur(self, direction):
+        # Get next coordinates with etat_suivant
+        next_etat = self.etat_suivant(self.position_joueur, direction)
+        # Check if next coordinates are in the map
+        print('next_etat :', next_etat)
+        print('matrice :', self.matrice)
+        if self.matrice[next_etat[0]][next_etat[1]] == 10:
+            print('Tresor trouvé !')
+            exit()
         if self.steps < self.max_steps:
             if direction == 'N':
                 if self.position_joueur[0] > 0:
+                    self.matrice[self.position_joueur[0]][self.position_joueur[1]] = 0
                     self.position_joueur[0] -= 1
                     self.steps += 1
             elif direction == 'S':
                 if self.position_joueur[0] < self.taille - 1:
+                    self.matrice[self.position_joueur[0]][self.position_joueur[1]] = 0
                     self.position_joueur[0] += 1
                     self.steps += 1
             elif direction == 'E':
                 if self.position_joueur[1] < self.taille - 1:
+                    self.matrice[self.position_joueur[0]][self.position_joueur[1]] = 0
                     self.position_joueur[1] += 1
                     self.steps += 1
             elif direction == 'O':
                 if self.position_joueur[1] > 0:
+                    self.matrice[self.position_joueur[0]][self.position_joueur[1]] = 0
                     self.position_joueur[1] -= 1
                     self.steps += 1
             else:
@@ -132,6 +188,7 @@ class Ile:
 
     def __str__(self):
         return str(self.matrice)
+
 
 # Implémentation de l'algorithme egreedy
 def egreedy(ile, epsilon):
@@ -152,21 +209,41 @@ def run_egreedy(ile):
         print("\n")
 
 def qlearning(ile):
-    qtable = []
-    for i in range(ile.taille):
-        qtable.append([])
-        for j in range(ile.taille):
-            qtable[i].append([])
-            for k in range(4):
-                qtable[i][j].append(0)
+    agent = Agent(0.9, ile)
+    choosen_path = []
+    for step in range(ile.max_steps):
+        print(ile)
+        print("Actions possibles: ", ile.actions_possibles())
+        action = agent.choisir_action(ile.position_joueur)
+        print("Action choisie: ", action)
+        choosen_path.append(action)
+        ile.mouvement_joueur(action)
+        print("Position du joueur: ", ile.position_joueur)
+        print("Nombre de pas: ", ile.steps)
+        reward = ile.matrice[ile.position_joueur[0]][ile.position_joueur[1]]
+        etat = ile.position_joueur
+        # determine action idx
+        action_idx = 0
+        if action == 'N':
+            action_idx = 0
+        elif action == 'S':
+            action_idx = 1
+        elif action == 'E':
+            action_idx = 2
+        elif action == 'O':
+            action_idx = 3
+        print("Reward: ", reward)
+        print("Path: ", choosen_path)
+        print(agent.q)
+        agent.update_q(etat, action_idx, reward)
+        print("\n")
 
     # Fill qtable
 
 def main():
-    ile = Ile(5)
+    ile = Ile(10)
     ile.generer_ile()
-    print(ile.matrice)
-    print(ile.calculer_matrice_etat_actions())
+    qlearning(ile)
     
 
 if __name__ == '__main__':
